b5fb37799abf9725a20e3e686df19c9d25969142
Mansi <mansgupt11@gmail.com>
2021-01-25 19:44:10 -0500

    added project skeleton

---
+++ src/test/java/com/softmaxsolutions_jenkinspipeline_project2/com/softmaxsolutions_jenkinspipeline_project2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmaxsolutions_jenkinspipeline_project2.com.softmaxsolutions_jenkinspipeline_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
